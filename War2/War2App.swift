5fb4924d1541046fe518e84d9e162cbfd28ed578
oniszczak <aleks42@gmail.com>
2023-01-08 00:27:03 -0500

    Initial Commit

---
+++ War2/War2App.swift
@@ -0,0 +1,17 @@
+//
+//  War2App.swift
+//  War2
+//
+//  Created by ALEKSANDER ONISZCZAK on 2023-01-08.
+//
+
+import SwiftUI
+
+@main
+struct War2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
